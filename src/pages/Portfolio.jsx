d7cc4e1fe384a22514e253adb0cca4bc3a5de100
B Sai Umesh <saiumesh2003@gmail.com>
2025-08-20 19:22:45 +0530

    Create Portfolio.jsx

---
+++ src/pages/Portfolio.jsx
@@ -0,0 +1,13 @@
+import Navbar from "../components/Navbar";
+import Sections from "../components/Sections";
+
+const Portfolio = () => {
+  return (
+    <div>
+      <Navbar />
+      <Sections />
+    </div>
+  );
+};
+
+export default Portfolio;
